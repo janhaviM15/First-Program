44a4d0c5112deaa3e29a4c0fe436bb5e57b4e45d
Janhavi Mali <167394492+janhaviM15@users.noreply.github.com>
2025-08-19 13:15:44 +0530

    Add files via upload

---
+++ FirstEx.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class FirstEx {
+
+	public static void main(String[] args)
+	{
+		String name = "John";
+		int phno = 456788;
+		float percentage = 88.2f ;
+		boolean passStatus = true;
+		char middleName = 'A';
+		
+		System.out.println("Hello");
+		System.out.print(name+" "+phno+" "+percentage+" "+passStatus+" "+middleName);
+	}
+
+}
+ 
